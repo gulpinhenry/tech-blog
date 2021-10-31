3666ca1c6b14db4501cc5364468a942c585db1f3
gulpinhenry <69442567+gulpinhenry@users.noreply.github.com>
2021-10-31 16:09:46 -0700

    add homepage getroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,4 +2,34 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const sequelize = require('../config/connection');
 
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            attributes: ["id", "title", "body", "user_id"],
+            include: [
+                {
+                    model: User,
+                    as: "user",
+                    attributes: ["username"],
+                },
+                {
+                    model: Comment,
+                    as: "comments",
+                    attributes: ["id", "comment_text", "user_id"],
+                },
+            ],
+        });
+        // serialize
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
