f76549dd7d1f30cd138c61eb8526de360080c055
gulpinhenry <69442567+gulpinhenry@users.noreply.github.com>
2021-11-01 00:02:03 -0700

    add postdelete routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -24,22 +24,82 @@ router.get("/", async (req, res) => {
         res.status(500).json(err);
     }
 });
-
+router.get("/:id", async (req, res) => {
+    try{
+        const postData = Post.findOne({
+            where: {
+                id: req.params.id,
+            },
+        })
+        if(!postData){
+            res.status(404).json({ message: "No Post found with this id" });
+            return;
+        }
+        res.json(postData);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 router.post("/", async (req, res) => {
-    // This will make a new post
-    // Expects Title, body, user_id
     try {
         const postData = await Post.create({
             title: req.body.title,
             content: req.body.content,
             user_id: req.session.user_id,
         })
-        res.status(200).json(dbPostData);
+        res.status(200).json(postData);
     }
     catch (err) {
         console.log(err);
-        res.status(500).json(err); //REST api needs status
+        res.status(500).json(err);
     }
 });
 
+//take in post id
+router.put("/:id", async (req, res) => {
+
+    try {
+        const postData = await Post.update(
+            {
+                title: req.body.title,
+                content: req.body.content,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        )
+        if (!postData) {
+            res.status(404).json({ message: "No Post found with this id" });
+            return;
+        }
+        res.json(postData);
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+});
+
+//TODO
+router.delete("/:id", async (req, res) => {
+    try{
+        const postData = Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+        if(!postData){
+            res.status(404).json({ message: "No Post found with this id" });
+            return;
+        }
+        res.json(postData);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
