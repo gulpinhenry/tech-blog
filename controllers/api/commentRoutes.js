f3f4eded29b5818cf12192bd792eb26cac9a4e87
gulpinhenry <69442567+gulpinhenry@users.noreply.github.com>
2021-11-02 16:12:57 -0700

    edit js files

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,1 +1,63 @@
-//module.exports = router;
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+router.get("/", async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            attributes: ["id", "comment_text", "user_id", "post_id"],
+            include: [
+                {
+                    model: User,
+                    as: "user",
+                    attributes: ["username"],
+                },
+            ],
+        })
+        res.json(commentData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const commentData = await Comment.findOne({
+            where: {
+                id: req.params.id,
+            },
+            attributes: ["id", "comment_text", "user_id", "post_id"],
+            include: [
+                {
+                    model: User,
+                    as: "user",
+                    attributes: ["username"],
+                },
+            ],
+        })
+        if (!commentData) {
+            res.status(404).json({ message: "No Comment found with this id" });
+            return;
+        }
+        res.json(commentData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+//expects comment_text, user_id, post_id
+router.post("/", (req, res) => {
+    try{
+        const commentData = Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id,
+          })
+          res.json(commentData);
+    } catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+  });
+module.exports = router;
--- public/js/script.js
+++ public/js/script.js
@@ -9,12 +9,16 @@ var loginForm = document.getElementById("login-form");
 var logout = document.getElementById("logout-btn");
 
 logout.addEventListener("click", function (event) {
+    console.log("hi");
     event.preventDefault();
     fetch(hostPrefix + '/api/users/logout', {
         method: "POST"
     })
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => {
+            console.log(json)
+            window.location.replace(hostPrefix + "/");
+        })
         .catch(err => console.log(err));
 });
 
@@ -31,12 +35,43 @@ login.addEventListener("click", function (event) {
         body: JSON.stringify(postData),
         headers: { "Content-type": "application/json; charset=UTF-8" }
     })
-        .then(response => response.json())
-        .then(json => console.log(json))
+        .then(response => {
+            console.log(response);
+            if (response.ok) {
+                console.log('success');
+                document.location.replace('/');
+    
+            } else {
+                window.alert("incorrect password");
+            }
+            
+        })
         .catch(err => console.log(err));
+        
+    
 });
 
 signup.addEventListener("click", function (event) {
     event.preventDefault();
+    let postData = {
+        username: signupForm.elements[0].value,
+        password: signupForm.elements[1].value,
+    }
+    console.log(JSON.stringify(postData));
+    fetch(hostPrefix + '/api/users', {
+        method: "POST",
+        body: JSON.stringify(postData),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log('success');
+                document.location.replace('/dashboard');
+            } else {
+                alert(response.statusText);
+            }
+            window.location.replace(hostPrefix + "/");
+        })
+        .catch(err => console.log(err));
 
 });
